82c9d5754230109e5f4e0e8341efddaae3f6de55
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2024-03-28 19:34:26 +0000

    [#5] Create calculator class

---
+++ src/calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+  double Add(double a, double b);
+  double Subtract(double a, double b);
+  double Multiply(double a, double b);
+  double Divide(double a, double b);
+};
+
+#endif // CALCULATOR_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include "calculator.h"
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  Calculator calc;
+  std::cout << "Add(5, 3): " << calc.Add(5, 3) << "\n";
+  std::cout << "Subtract(5, 3): " << calc.Subtract(5, 3) << "\n";
+  std::cout << "Multiply(5, 3): " << calc.Multiply(5, 3) << "\n";
+  std::cout << "Divide(5, 3): " << calc.Divide(5, 3) << "\n";
+  try {
+    std::cout << "Divide(5, 0): " << calc.Divide(5, 0)
+              << "\n"; // This will throw
+  } catch (const std::runtime_error &e) {
+    std::cout << e.what() << "\n";
+  }
   return 0;
 }
