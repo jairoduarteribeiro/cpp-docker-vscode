a55544d892f89c3a4f3379859fd56bb6c2a0eb3e
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2024-03-28 19:36:56 +0000

    [#5] Change maximum line length to 120

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,7 @@ int main() {
   std::cout << "Multiply(5, 3): " << calc.Multiply(5, 3) << "\n";
   std::cout << "Divide(5, 3): " << calc.Divide(5, 3) << "\n";
   try {
-    std::cout << "Divide(5, 0): " << calc.Divide(5, 0)
-              << "\n"; // This will throw
+    std::cout << "Divide(5, 0): " << calc.Divide(5, 0) << "\n"; // This will throw
   } catch (const std::runtime_error &e) {
     std::cout << e.what() << "\n";
   }
